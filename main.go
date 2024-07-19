b81065fc5096747cb1296c480dcbd3f78ac1a24b
Laith <laithsaqqa@gmail.com>
2024-07-20 02:47:51 +0300

    Find unsolved problems

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	models "cf-problem-finder/model"
 	services "cf-problem-finder/service"
 	"fmt"
 )
 
 func main() {
 	allProblems := services.FetchAllProblems()
-	fmt.Println(allProblems[0].Id())
+	fmt.Println(len(allProblems))
+
 	mySolvedProblems := services.FetchSolvedProblemsForUser("NapSaq")
 	fmt.Println(len(mySolvedProblems))
 
+	solvedProblems := make(map[string]bool)
+	for _, problem := range mySolvedProblems {
+		solvedProblems[problem.Id()] = true
+	}
+
+	fmt.Println(len(solvedProblems))
+
+	unsolvedProblems := []models.Problem{}
+	for _, problem := range allProblems {
+		if !solvedProblems[problem.Id()] {
+			unsolvedProblems = append(unsolvedProblems, problem)
+		}
+	}
+
+	fmt.Println(len(unsolvedProblems))
 }
