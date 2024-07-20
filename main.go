31274e1b03ffae6ea0a862b348064ff0d36cf826
Laith <laithsaqqa@gmail.com>
2024-07-20 04:35:55 +0300

    Pick problems for specific ratings

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"sort"
 )
 
+var TargetRatings = []int{800, 1000, 1200, 1400, 1600, 1900}
+
 func main() {
 	allProblems := services.FetchAllProblems()
 	fmt.Printf("# of all Problems:\t\t%d\n", len(allProblems))
@@ -30,6 +32,27 @@ func main() {
 		return unsolvedProblems[i].SolvedCount > unsolvedProblems[j].SolvedCount
 	})
 
-	fmt.Println(unsolvedProblems[2])
+	pickedProblems := make(map[int]models.Problem)
+	for _, problem := range unsolvedProblems {
+		for _, targetRating := range TargetRatings {
+			if _, found := pickedProblems[targetRating]; found {
+				continue
+			}
+			if problem.Rating == targetRating {
+				pickedProblems[targetRating] = problem
+				break
+			}
+		}
+		if len(pickedProblems) == len(TargetRatings) {
+			break
+		}
+	}
+	fmt.Printf("Successfully picked %d Problems!\n", len(pickedProblems))
+	for _, rating := range TargetRatings {
+		problem := pickedProblems[rating]
+		fmt.Printf("%d - Problem %s: %s\n", rating, problem.Id(), problem.Name)
+		fmt.Println(problem)
+		fmt.Println("---")
+	}
 
 }
