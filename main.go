1867a7408050791f4583c89fb680d8dd2450a1e5
Laith <laithsaqqa@gmail.com>
2024-09-25 18:47:51 +0300

    Enabled dublicate ratings picks and fix naming to follow convention

--- main.go
+++ main.go
@@ -5,20 +5,21 @@ import (
 	services "cf-problem-finder/service"
 	"fmt"
 	"sort"
+	"slices"
 )
 
-// var TargetRatings = []int{800, 1000, 1200, 1400, 1600, 1900}
+// var targetRatings = []int{800, 1000, 1200, 1400, 1600, 1900}
 
 func main() {
 	userHandles := services.ReadUserHandles()
 	fmt.Printf("Succesfully parced %d users!\n", len(userHandles))
 
-	TargetRatings := services.ReadTargetRatings()
-	fmt.Printf("Succesfully parced %d target ratings!\n", len(TargetRatings))
-	fmt.Printf("Finding problems rated at: %v\n", TargetRatings)
+	targetRatings := services.ReadTargetRatings()
+	fmt.Printf("Succesfully parced %d target ratings!\n", len(targetRatings))
+	fmt.Printf("Finding problems rated at: %v\n", targetRatings)
 
-	// assert that len(TargetRatings) > 0
-	if len(TargetRatings) == 1 && TargetRatings[0] == 0 {
+	// assert that len(targetRatings) > 0
+	if len(targetRatings) == 1 && targetRatings[0] == 0 {
 		panic("No proper target ratings found!")
 	}
 
@@ -40,25 +41,26 @@ func main() {
 		return unsolvedProblems[i].SolvedCount > unsolvedProblems[j].SolvedCount
 	})
 
-	pickedProblems := make(map[int]models.Problem)
-	for _, problem := range unsolvedProblems {
-		for _, targetRating := range TargetRatings {
-			if _, found := pickedProblems[targetRating]; found {
+	pickedProblems := make([]models.Problem, len(targetRatings))
+	pickedProblemsCount := 0
+
+	for ratingIndex, targetRating := range targetRatings {
+		for _, problem := range unsolvedProblems {
+			if i := slices.IndexFunc(pickedProblems, func(p models.Problem) bool { return p.Id() == problem.Id() }); i != -1 {
+				// skip if problem is already picked
 				continue
 			}
 			if problem.Rating == targetRating {
-				pickedProblems[targetRating] = problem
+				pickedProblems[ratingIndex] = problem
+				pickedProblemsCount++
 				break
 			}
 		}
-		if len(pickedProblems) == len(TargetRatings) {
-			break
-		}
 	}
-	fmt.Printf("Successfully picked %d/%d Problems!\n", len(pickedProblems), len(TargetRatings))
-	for _, rating := range TargetRatings {
-		problem, found := pickedProblems[rating]
-		if !found {
+	fmt.Printf("Successfully picked %d/%d Problems!\n", pickedProblemsCount, len(targetRatings))
+	for ratingIndex, rating := range targetRatings {
+		problem := pickedProblems[ratingIndex]
+		if problem.Id() == (models.Problem{}).Id() {
 			fmt.Printf("%d - No problem found!\n", rating)
 		} else {
 			fmt.Printf("%d - Problem %s : %s\n", rating, problem.Id(), problem.Name)
